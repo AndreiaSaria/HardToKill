5bebd8bfa99551b6b486a0448fc00049aefcf1cf
Andréia/Saria <63603210+AndreiaSaria@users.noreply.github.com>
2020-05-22 15:18:46 -0300

    Added some textures and UI + enemy not dying
    Problem on enemy not dying

--- Assets/Scripts/HPCount.cs
+++ Assets/Scripts/HPCount.cs
@@ -1,24 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class HPCount : MonoBehaviour
 {
     public int HP;
-    
+    //colei daqui https://answers.unity.com/questions/1633697/cant-drag-slider-onto-inspector.html
+    public UnityEngine.UI.Slider sliderHp;
+
+    private void OnBecameVisible()
+    {
+        sliderHp.value = HP;
+    }
+
     public void Damaged(int i) //Desse jeito que fiz não dá para ser estático. That just happened.
     {//Sei que passar mensagens não é a melhor forma de se fazer isso. That just happened²
 
         HP = HP - i;
         //Debug.Log("Hp dropped " + i + "values");
+        sliderHp.value = HP;
 
         if (HP <= 0)
         {
             SendMessage("Death");
+            sliderHp.gameObject.SetActive(false);
 
             if(gameObject.tag != "Player")
             {
-                Destroy(gameObject, 4f);
+                Destroy(this.gameObject, 4f);
             }
         }
         else
